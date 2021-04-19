6751146c1cda502e181a7aad65c764390ab478aa
Eduardo Meli <80851293+edu-m@users.noreply.github.com>
2021-04-19 20:27:26 +0200

    Add files via upload

---
+++ snonks.cpp
@@ -0,0 +1,237 @@
+#include <SFML/Graphics.hpp>
+#include <time.h>
+#include <windows.h>
+using namespace sf;
+using namespace std;
+
+int i1;
+int i2;
+int N = 70, M = 38;
+int sz = 8;
+int w = sz * N;
+int h = sz * M;
+double delay;
+int dir = 1, num = 4;
+
+bool pause = false;
+
+struct Snake
+{
+	int x, y;
+}  s[150];
+
+struct Fruit
+{
+	int x, y;
+} f;
+void Direction()
+{
+	if (Keyboard::isKeyPressed(Keyboard::Left)) //direzionamento
+	{
+		if (dir == 3)
+		{
+			dir = 1;
+		}
+		else if (dir == 0)
+		{
+			dir = 1;
+		}
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Right))
+	{
+		if (dir == 3)
+		{
+			dir = 2;
+		}
+		else if (dir == 0)
+		{
+			dir = 2;
+		}
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Up))
+	{
+		if (dir == 1)
+		{
+			dir = 3;
+		}
+		else if (dir == 2)
+		{
+			dir = 3;
+		}
+	}
+	if (Keyboard::isKeyPressed(Keyboard::Down))
+	{
+		if (dir == 1)
+		{
+			dir = 0;
+		}
+		else if (dir == 2)
+		{
+			dir = 0;
+		}
+	}
+	if ((Keyboard::isKeyPressed(Keyboard::Space))) //evento pausa
+	{
+		pause = true;
+	}
+	if ((Keyboard::isKeyPressed(Keyboard::Enter)) && (pause == true))
+	{
+		pause = false;
+	}
+}
+
+void Tick()
+{
+	if (pause == false)
+	{
+		for (int i1 = num; i1 > 0; --i1)
+		{
+			s[i1].x = s[i1 - 1].x;
+			s[i1].y = s[i1 - 1].y;
+		}
+
+		if (dir == 0) s[0].y += 1;
+		if (dir == 1) s[0].x -= 1;
+		if (dir == 2) s[0].x += 1;
+		if (dir == 3) s[0].y -= 1;
+
+		if ((s[0].x == f.x) && (s[0].y == f.y)) //evento frutto mangiato
+		{
+			num++;
+			f.x = rand() % N;
+			f.y = rand() % M;
+		}
+
+		if (s[0].x > N) s[0].x = 0;  if (s[0].x < 0) s[0].x = N; //wraparound
+		if (s[0].y > M) s[0].y = 0;  if (s[0].y < 0) s[0].y = M;
+
+
+		for (int i2 = 1; i2 < num; i2++)
+		{
+			if (s[0].x == s[i2].x && s[0].y == s[i2].y) //evento morte
+			{
+				num = 4;
+				delay = 0.1;
+				Sleep(3000);
+				s[0].x = rand() % N;
+				s[0].y = rand() % M;
+				dir = 2;
+			}
+		}
+	}
+	else
+	{
+
+	}
+}
+
+int main()
+{
+	FreeConsole();
+	RenderWindow window(VideoMode(w, h), "Snake");
+
+	Texture t1, t2, t3;
+	t1.loadFromFile("images/tile.png");
+	t2.loadFromFile("images/snake.png");
+	t3.loadFromFile("images/food.png");
+
+	Sprite sprite1(t1);
+	Sprite sprite2(t2);
+	Sprite sprite3(t3);
+
+	Clock clock;
+	float timer = 0;
+
+	Font font;
+	font.loadFromFile("pixelated.ttf");
+
+	Text text;
+	text.setCharacterSize(24);
+	text.setPosition(540,2);
+	text.setFillColor(sf::Color::Black);
+
+	f.x = 10;
+	f.y = 10;
+
+	while (window.isOpen())
+	{
+		float time = clock.getElapsedTime().asSeconds();
+		clock.restart();
+
+			timer += time;
+
+			Direction();
+
+			Event e;
+			while (window.pollEvent(e))
+			{
+				if (e.type == Event::Closed)
+					window.close();
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::Escape))
+			{
+				return 0;
+			}
+
+			if (num < 19) //velocit� in relazione a lunghezza
+			{
+				delay = 0.1;
+			}
+			if (num == 27)
+			{
+				delay = 0.07;
+			}
+			if (num == 39)
+			{
+				delay = 0.05;
+			}
+			if (num == 56)
+			{
+				delay = 0.03;
+			}
+			if (num == 69)
+			{
+				delay = 0.015;
+			}
+
+			if (timer > delay)
+			{
+				timer = 0;
+				Tick();
+			}
+
+			window.clear();
+
+			for (int i = 0; i < N; i++) //disegno sprite
+			{
+				for (int j = 0; j < M; j++)
+				{
+					sprite1.setPosition(i * sz, j * sz);  window.draw(sprite1);
+				}
+
+				for (int i = 0; i < num; i++)
+				{
+					sprite2.setPosition(s[i].x * sz, s[i].y * sz);  window.draw(sprite2);
+				}
+			}
+
+			text.setFont(font); //conteggio punti
+			int score = num - 4;
+			string str = to_string(score);
+			text.setString(str);
+			if(score>=10)
+			{
+				text.setPosition(535, 2);
+			}
+			else
+			{
+				text.setPosition(540, 2);
+			}
+
+			sprite3.setPosition(f.x * sz, f.y * sz);  window.draw(sprite3);
+			window.draw(text);
+			window.display();
+	}
+	return 0;
+}
