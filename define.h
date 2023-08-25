6db07b350e0db7a0c55df446b018dce9984fd6c0
edu-m <mledrd02e15c351o@studium.unict.it>
2023-08-25 16:49:59 +0200

    first implementation v2

---
+++ define.h
@@ -0,0 +1,16 @@
+#pragma once
+//config
+#define HOR_SIZE 70
+#define VER_SIZE 47
+#define WINDOW_SCALE 8
+#define IS_KEY_PRESSED(arg) Keyboard::isKeyPressed(Keyboard::arg)
+#define WINDOW_FRAMERATE 60
+
+//snake
+#define SPEED1 0.1
+#define SPEED2 0.07
+#define SPEED3 0.05
+#define SPEED4 0.03
+#define SPEED5 0.015
+#define SNAKE_MAX_SIZE 150
+#define DEFAULT_SNAKE_SIZE 4
--- main.cpp
+++ main.cpp
@@ -1,227 +1,165 @@
+#include "define.h"
+#include "fruit.h"
+#include "snake.h"
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
 #include <cstdint>
+#include <cstdlib>
 #include <time.h>
 #include <unistd.h>
+#include <fcntl.h>
 using namespace sf;
 using namespace std;
+RenderWindow *initWindow() {
 
-#define SPEED1 0.1
-#define SPEED2 0.07
-#define SPEED3 0.05
-#define SPEED4 0.03
-#define SPEED5 0.015
-
-#define IS_KEY_PRESSED(arg) Keyboard::isKeyPressed(Keyboard::arg)
-
-int i1;
-int i2;
-int N = 70, M = 47;
-int sz = 8;
-int w = sz * N;
-int h = sz * M;
-double delay = SPEED1;
-int dir = 1, snakeSize = 4;
-bool overrideSpeed = false;
-Clock clock1;
-
-#define DOWN 0
-#define LEFT 1
-#define RIGHT 2
-#define UP 3
-
-bool containedWithin(int, int, int &);
-void overrideSpeedFunc(const double &);
-void Direction();
-void wait();
-void checkSpeed();
-void Tick();
-
-bool _pause = false;
-
-struct Snake {
-  int x, y;
-} s[150];
-
-struct Fruit {
-  int x, y;
-} f;
-
-bool containedWithin(int lBound, int hBound, int &snakeSize) {
-  return (snakeSize < hBound && snakeSize > lBound);
+  RenderWindow *window = new RenderWindow(
+      VideoMode(WINDOW_SCALE * HOR_SIZE, WINDOW_SCALE * VER_SIZE), "Snake");
+  window->setFramerateLimit(WINDOW_FRAMERATE);
+  return window;
 }
 
-void overrideSpeedFunc(const double &speed) { delay = speed; }
-
-void Direction() {
-  if ((IS_KEY_PRESSED(Keyboard::Left) || IS_KEY_PRESSED(Keyboard::A)) &&
-      (dir == UP || dir == DOWN)) // direction control
-    dir = LEFT;
-  if ((IS_KEY_PRESSED(Keyboard::Right) || IS_KEY_PRESSED(Keyboard::D)) &&
-      (dir == UP || dir == DOWN))
-    dir = RIGHT;
-  if ((IS_KEY_PRESSED(Keyboard::Up) || IS_KEY_PRESSED(Keyboard::W)) &&
-      (dir == LEFT || dir == RIGHT))
-    dir = UP;
-  if ((IS_KEY_PRESSED(Keyboard::Down)) ||
-      ((IS_KEY_PRESSED(Keyboard::S)) && (dir == LEFT || dir == RIGHT)))
-    dir = DOWN;
-  if ((IS_KEY_PRESSED(Keyboard::Space))) //_pause event
-  {
-    _pause = !_pause;
-    sleep(1); // functions as debounce when paused, a time for the player to get
-              // ready when unpaused
+void windowEventHandler(Event &e, RenderWindow *window) {
+  while (window->pollEvent(e)) {
+    if (e.type == Event::Closed)
+      window->close();
   }
-}
-void checkSpeed() {
-  if (containedWithin(1, 19, snakeSize))
-    delay = SPEED1;
-  else if (containedWithin(19, 27, snakeSize))
-    delay = SPEED2;
-  else if (containedWithin(27, 39, snakeSize))
-    delay = SPEED3;
-  else if (containedWithin(39, 56, snakeSize))
-    delay = SPEED4;
-  if (snakeSize >= 56)
-    delay = SPEED5;
+  if (IS_KEY_PRESSED(Keyboard::Escape))
+    exit(0);
 }
 
 void checkChangeDirectionEvent() {
-  if (dir == DOWN)
-    s[0].y += 1;
-  if (dir == LEFT)
-    s[0].x -= 1;
-  if (dir == RIGHT)
-    s[0].x += 1;
-  if (dir == UP)
-    s[0].y -= 1;
+  Snake &snake = Snake::getInstance();
+  if (snake.getDirection() == Direction::down)
+    snake.setYPos(snake.getPos()->at(0).second + 1);
+  if (snake.getDirection() == Direction::left)
+    snake.setXPos(snake.getPos()->at(0).first - 1);
+  if (snake.getDirection() == Direction::right)
+    snake.setXPos(snake.getPos()->at(0).first + 1);
+  if (snake.getDirection() == Direction::up)
+    snake.setYPos(snake.getPos()->at(0).second - 1);
 }
 
-void checkFruitEatenEvent() {
-  if ((s[0].x == f.x) && (s[0].y == f.y)) // fruit eaten event
+void getUserInput() {
+  Snake &snake = Snake::getInstance();
+  if (IS_KEY_PRESSED(Keyboard::Left)) // direction control
+    snake.setDirection(Direction::left);
+  if (IS_KEY_PRESSED(Keyboard::Right))
+    snake.setDirection(Direction::right);
+  if (IS_KEY_PRESSED(Keyboard::Up))
+    snake.setDirection(Direction::up);
+  if (IS_KEY_PRESSED(Keyboard::Down))
+    snake.setDirection(Direction::down);
+  if ((IS_KEY_PRESSED(Keyboard::Space))) //_pause event
   {
-    ++snakeSize;
-    f.x = rand() % N;
-    f.y = rand() % M;
-    checkSpeed();
+    snake.togglePauseGame();
+    usleep(500000);
   }
 }
 
 void checkWraparound() {
-  if (s[0].x > N)
-    s[0].x = 0;
-  if (s[0].x < 0)
-    s[0].x = N; // wraparound
-  if (s[0].y > M)
-    s[0].y = 0;
-  if (s[0].y < 0)
-    s[0].y = M;
+  Snake &snake = Snake::getInstance();
+  // cout << snake.getPos()->at(0).first << " " << snake.getPos()->at(0).second
+  //      << endl;
+
+  if (snake.getPos()->at(0).first > HOR_SIZE)
+    snake.setXPos(0);
+  if (snake.getPos()->at(0).first < 0)
+    snake.setXPos(HOR_SIZE);
+  if (snake.getPos()->at(0).second > VER_SIZE)
+    snake.setYPos(0);
+  if (snake.getPos()->at(0).second < 0)
+    snake.setYPos(VER_SIZE);
+}
+
+void checkFruitEatenEvent() {
+  Snake &snake = Snake::getInstance();
+  Fruit &fruit = Fruit::getInstance();
+  if (snake.getPos()->at(0) == fruit.getPos()) // fruit eaten event
+  {
+    snake.setSize(snake.getSize() + 1);
+    fruit.generateRandomPosition();
+  }
 }
 
 void checkDeathEvent() {
-  for (int i2 = 1; i2 < snakeSize; i2++)
-    if (s[0].x == s[i2].x && s[0].y == s[i2].y) // death event
-    {
-      snakeSize = 4;
-      sleep(4);
-      s[0].x = rand() % N;
-      s[0].y = rand() % M;
-      dir = rand() % 4 + 1; // random direction
+  Snake &snake = Snake::getInstance();
+  for (int i = 1; i < snake.getSize(); i++)
+    if (snake.getPos()->at(0).first == snake.getPos()->at(i).first &&
+        snake.getPos()->at(0).second == snake.getPos()->at(i).second) {
+      snake.setSize(4);
+      sleep(3);
+      snake.generateRandomPosition();
+      snake.setDirection(Direction::right);
     }
 }
 
 void Tick() {
-  if (!_pause) {
-    for (int i1 = snakeSize; i1 > 0; --i1) {
-      s[i1].x = s[i1 - 1].x;
-      s[i1].y = s[i1 - 1].y;
-    }
-
+  Snake &snake = Snake::getInstance();
+  getUserInput();
+  if (!snake.isGamePaused()) {
+    for (int i = snake.getSize(); i > 0; i--)
+      snake.setPosIndex(snake.getPos()->at(i - 1), i);
     checkChangeDirectionEvent();
-
     checkFruitEatenEvent();
-
     checkWraparound();
-
     checkDeathEvent();
   }
 }
+void drawWindow(Sprite &tileSprite, Sprite &snakeSprite, Sprite &foodSprite,
+                RenderWindow *window) {
 
-int main() {
-  RenderWindow window(VideoMode(w, h), "Snake");
+  Snake &snake = Snake::getInstance();
+  Fruit &fruit = Fruit::getInstance();
+  window->clear();
+  for (int i = 0; i < HOR_SIZE; i++) // sprite drawing function
+  {
+    for (int j = 0; j < VER_SIZE; j++) {
+      tileSprite.setPosition(i * WINDOW_SCALE, j * WINDOW_SCALE);
+      window->draw(tileSprite);
+    }
+    vector<pair<int, int>> *snakePos = snake.getPos();
+    for (int i = 0; i < snake.getSize(); i++) {
+      snakeSprite.setPosition(snakePos->at(i).first * WINDOW_SCALE,
+                              snakePos->at(i).second * WINDOW_SCALE);
+      window->draw(snakeSprite);
+    }
+    foodSprite.setPosition(fruit.getPos().first * WINDOW_SCALE,
+                           fruit.getPos().second * WINDOW_SCALE);
+    window->draw(foodSprite);
+  }
+  window->display();
+}
 
-  Texture t1, t2, t3;
-  t1.loadFromFile("images/tile.png");
-  t2.loadFromFile("images/snake.png");
-  t3.loadFromFile("images/food.png");
+void mainLoopGame() {
+  float time, timer = 0;
+  Event e;
+  Clock gameClock;
+  Sprite tileSprite, snakeSprite, foodSprite;
 
-  Sprite sprite1(t1);
-  Sprite sprite2(t2);
-  Sprite sprite3(t3);
+  RenderWindow *window = initWindow();
+  Snake &snake = Snake::getInstance();
 
-  float timer = 0;
+  Texture tileTexture, snakeTexture, foodTexture;
+  tileTexture.loadFromFile("images/tile.png");
+  snakeTexture.loadFromFile("images/snake.png");
+  foodTexture.loadFromFile("images/food.png");
 
-  Font font;
-  font.loadFromFile("pixelated.ttf");
+  tileSprite = Sprite(tileTexture);
+  snakeSprite = Sprite(snakeTexture);
+  foodSprite = Sprite(foodTexture);
 
-  Text text;
-  text.setCharacterSize(sz*5);
-  text.setFillColor(sf::Color::Black);
-
-  f.x = 10;
-  f.y = 10;
-  sprite1.scale(float(sz) / 8, float(sz) / 8);
-  sprite2.scale(float(sz) / 8, float(sz) / 8);
-  sprite3.scale(float(sz) / 8, float(sz) / 8);
-  while (window.isOpen()) {
-    float time = clock1.getElapsedTime().asSeconds();
-    clock1.restart();
-
+  while (window->isOpen()) {
+    time = gameClock.getElapsedTime().asSeconds();
+    gameClock.restart();
     timer += time;
 
-    Direction();
-    window.setFramerateLimit(30);
-    Event e;
-    while (window.pollEvent(e)) {
-      if (e.type == Event::Closed)
-        window.close();
-    }
-
-    if (IS_KEY_PRESSED(Keyboard::Escape))
-      return 0;
-
-    if (timer > delay) {
+    windowEventHandler(e, window);
+    if (timer > snake.getSpeed()) {
       timer = 0;
       Tick();
     }
-
-    window.clear();
-
-    for (int i = 0; i < N; i++) // sprite drawing function
-    {
-      for (int j = 0; j < M; j++) {
-        sprite1.setPosition(i * sz, j * sz);
-        window.draw(sprite1);
-      }
-
-      for (int i = 0; i < snakeSize; i++) {
-        sprite2.setPosition(s[i].x * sz, s[i].y * sz);
-        window.draw(sprite2);
-      }
-    }
-
-    text.setFont(font); // score count
-    int score = snakeSize - 4;
-    string str = to_string(score);
-    text.setString(str);
-    if (score >= 10)
-      text.setPosition(sz*N-(sz*3), 2);
-    else
-      text.setPosition(sz*N-(sz*3), 2);
-
-    sprite3.setPosition(f.x * sz, f.y * sz);
-    window.draw(sprite3);
-    window.draw(text);
-    window.display();
+    drawWindow(tileSprite, snakeSprite, foodSprite, window);
   }
-  return 0;
 }
+
+int main() { mainLoopGame(); }
